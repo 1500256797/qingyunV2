a34c55ce4ee04cfedcb55d558bcd8b9ddbc62733
1500256797 <1500256797@qq.com>
2017-12-03 14:11:06 +0800

    优化页面

--- qingyunv2/src/main/java/com/qingyunqifu/controller/FileUploadController.java
+++ qingyunv2/src/main/java/com/qingyunqifu/controller/FileUploadController.java
@@ -11,22 +11,24 @@ import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-@RestController
+@Controller
 @MultipartConfig
 public class FileUploadController {
     @RequestMapping(value = "/upload/img" , method = RequestMethod.POST)
-    public Result  upload(HttpServletRequest servletRequest,
+    @ResponseBody
+    public Map<String, Object>  upload(HttpServletRequest servletRequest,
                          @RequestParam("file") MultipartFile file
                          ) throws IOException {
 
         //如果文件内容不为空，则写入上传路径
         if (!file.isEmpty()) {
             //上传文件路径
-            String path = servletRequest.getServletContext().getRealPath("/upload_img");
+            String path = "/home/jian/Desktop/qingyunV2/qingyunv2/src/main/resources/static/images";
 
             System.out.println("文件名称"+file.getOriginalFilename());
-            System.out.println("上传路径"+path);
             //上传文件名
             String filename = file.getOriginalFilename();
             File filepath = new File(path, filename);
@@ -40,8 +42,9 @@ public class FileUploadController {
             //将上传文件保存到一个目标文档中
             File file1 = new File(path + File.separator + filename);
             file.transferTo(file1);
-            return Result.success();
-
+            Map<String, Object> res = new HashMap<>();
+            res.put("url", file1);
+            return res;
 
         } else {
             return Result.failure();
